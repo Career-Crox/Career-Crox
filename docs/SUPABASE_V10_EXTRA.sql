713b08d279045d8e8ca01f92d704110528e761f5
Career-Crox <aaryansh.careercrox@gmail.com>
2026-03-18 01:57:46 +0530

    Delete docs directory

--- docs/SUPABASE_V10_EXTRA.sql
+++
@@ -1,8 +0,0 @@
--- Extra table for single-session login support
-create table if not exists active_sessions (
-  username text primary key,
-  session_token text,
-  ip_address text,
-  user_agent text,
-  updated_at text
-);
